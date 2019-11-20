19d3a127d8b1d256139a68c5967d82b79f36606f
vishu12 <35062673+vishu12@users.noreply.github.com>
2019-11-20 16:26:34 +0530

    Update a.py

--- a.py
+++ a.py
@@ -3,11 +3,12 @@
   
 num1 = 15
 num2 = 12
+num3 = 10
   
 # Adding two nos 
-sum = num1 + num2 
+sum = num1 + num2 + num3
   
 # printing values 
-print("Sum of {0} and {1} is {2}" .format(num1, num2, sum)) 
+print("Sum of {0} and {1} {2} is {3}" .format(num1, num2, num3, sum)) 
 
 
